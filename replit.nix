0481f145477871d61788981b60be879790d503cf
D4rkPK <>
2023-05-12 20:19:43 +0000

    update repo

--- replit.nix
+++ replit.nix
@@ -1,6 +1,7 @@
 { pkgs }: {
 	deps = [
-		pkgs.nodejs-12_x
+		pkgs.openssh_with_kerberos
+  pkgs.nodejs-12_x
 		pkgs.nodePackages.typescript-language-server
 		pkgs.yarn
 		pkgs.replitPackages.jest
--- src/client/Client.js
+++ src/client/Client.js
@@ -1,20 +1,20 @@
-const {Client, Collection, GatewayIntentBits} = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 
 module.exports = class extends Client {
   constructor(config) {
     super({
       intents: [
-    		GatewayIntentBits.Guilds,
-    		GatewayIntentBits.GuildMessages,
-    		GatewayIntentBits.MessageContent,
-    		GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates,
         GatewayIntentBits.GuildMessageReactions,
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildMessageTyping,
         GatewayIntentBits.DirectMessages,
         GatewayIntentBits.DirectMessageReactions,
         GatewayIntentBits.DirectMessageTyping
-	     ],
+      ],
     });
 
     this.commands = new Collection();
